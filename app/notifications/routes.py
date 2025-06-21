a1aca194d3935a244accc80c5c329775b3853013
andrew-scorchsoft <33485512+andrew-scorchsoft@users.noreply.github.com>
2025-06-21 12:10:26 +0300

    feat: add resubscribe notifications

--- app/notifications/routes.py
+++ app/notifications/routes.py
@@ -12,4 +12,18 @@ def unsubscribe(token: str):
         abort(404)
     member.email_opt_out = True
     db.session.commit()
-    return render_template('notifications/unsubscribed.html', member=member)
+    return render_template(
+        'notifications/unsubscribed.html', member=member, token=token
+    )
+
+
+@bp.route('/resubscribe/<token>')
+def resubscribe(token: str):
+    """Allow a member to opt back in to notification emails."""
+    token_obj = UnsubscribeToken.query.filter_by(token=token).first_or_404()
+    member = db.session.get(Member, token_obj.member_id)
+    if member is None:
+        abort(404)
+    member.email_opt_out = False
+    db.session.commit()
+    return render_template('notifications/resubscribed.html', member=member)
