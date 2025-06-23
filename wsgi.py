47f9f49822ae3e8b0a0d6693d7269f40503734a1
andrew-scorchsoft <33485512+andrew-scorchsoft@users.noreply.github.com>
2025-06-23 10:22:15 +0100

    fix: default to production config

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,10 @@
+import os
 from app import create_app
 
-app = create_app()
 
-if __name__ == '__main__':
-    app.run()
+config_path = os.getenv("APP_CONFIG", "config.ProductionConfig")
+app = create_app(config_path)
+
+if __name__ == "__main__":
+    debug_mode = config_path.endswith("DevelopmentConfig")
+    app.run(debug=debug_mode)
