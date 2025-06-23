cf01fad559061a6ade824a4eb2e8d7f318afc80b
andrew-scorchsoft <33485512+andrew-scorchsoft@users.noreply.github.com>
2025-06-23 10:43:47 +0100

    chore: merge migration heads

---
+++ migrations/versions/924e8580ef03_merge_password_token_branch.py
@@ -0,0 +1,24 @@
+"""merge password token branch
+
+Revision ID: 924e8580ef03
+Revises: 0a54760cfa78, u2v3w4x5
+Create Date: 2025-06-23 09:31:58.655238
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '924e8580ef03'
+down_revision = ('0a54760cfa78', 'u2v3w4x5')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
